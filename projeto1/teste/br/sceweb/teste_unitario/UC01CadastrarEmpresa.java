6a240cfe00f5b5af64f1c1ac7283be30b1d4a3ad
Lab103 <Lab103@LAB101-08>
2017-10-21 11:21:51 -0200

    Teste nome em branco

--- projeto1/teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
+++ projeto1/teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
@@ -15,8 +15,11 @@ import com.mysql.jdbc.exceptions.MySQLSyntaxErrorException;
 import br.sceweb.model.Empresa;
 import br.sceweb.model.EmpresaDAO;
 import br.sceweb.servico.ConfiguraDB;
+
 /**
- * Este script de teste verifica o comportamento do caso de uso UC01CadastrarEmpresa
+ * Este script de teste verifica o comportamento do caso de uso
+ * UC01CadastrarEmpresa
+ * 
  * @author professor
  *
  */
@@ -25,13 +28,14 @@ public class UC01CadastrarEmpresa {
 	static Empresa empresa;
 	static ConfiguraDB configuraDB;
 	static ConfiguraDB configuraDB1;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		String url = "jdbc:mysql://localhost/sceweb";
 		String driver = "com.mysql.jdbc.Driver";
 		String usuario = "root";
 		String senha = "alunofatec";
-		configuraDB = new ConfiguraDB(url, driver,usuario,senha);
+		configuraDB = new ConfiguraDB(url, driver, usuario, senha);
 		empresaDAO = new EmpresaDAO(configuraDB);
 		empresa = new Empresa();
 		empresa.setNomeDaEmpresa("empresa x");
@@ -39,37 +43,50 @@ public class UC01CadastrarEmpresa {
 		empresa.setNomeFantasia("empresa x");
 		empresa.setEndereco("rua taquari");
 		empresa.setTelefone("2222");
+		
 	}
+
 	/**
-	 * ID - CT01UC01FBCadastra_com_sucesso
-	 * Objetivo - verificar o comportamento do sistema na inclusao de empresa com 
-	 * sucesso
-	 * Pré-condiçao - O cnpj 89424232000180 nao esta cadastrado.
+	 * ID - CT01UC01FBCadastra_com_sucesso Objetivo - verificar o comportamento
+	 * do sistema na inclusao de empresa com sucesso Pré-condiçao - O cnpj
+	 * 89424232000180 nao esta cadastrado.
 	 */
 	@Test
 	public void CT01UC01FBCadastra_com_sucesso() {
 		String url = "jdbc:mysql://localhost/sceweb";
-		assertEquals(1,empresaDAO.adiciona(empresa));
+		assertEquals(1, empresaDAO.adiciona(empresa));
 	}
-	
+
 	@Test
 	public void CT02UC01FBCadastra_cnpj_invalido() {
 		empresaDAO = new EmpresaDAO(configuraDB);
-		assertEquals("CNPJ invalido.",empresa.setCnpj("89424"));
+		assertEquals("CNPJ invalido.", empresa.setCnpj("89424"));
 	}
+
 	@Test
 	public void CT03UC01FBCadastra_cnpj_ja_cadastrado() {
 		empresaDAO = new EmpresaDAO(configuraDB);
 		empresaDAO.adiciona(empresa);
-		assertEquals(0,empresaDAO.adiciona(empresa));
-		
+		assertEquals(0, empresaDAO.adiciona(empresa));
+
 	}
 	
+	@Test
+	public void CT04UC01FBCadastra_nome_invalido() {
+		empresaDAO = new EmpresaDAO(configuraDB);
+//		empresaDAO.adiciona(empresa);
+		assertEquals("Nome invalido", empresa.setNomeDaEmpresa(""));
+	}
+	
+	@Test
+	public void CT05UC01FBCadastra_Telefone() {
+		empresaDAO = new EmpresaDAO(configuraDB);
+//		empresaDAO.adiciona(empresa);
+		assertEquals("Telefone Invalido", empresa.setTelefone(""));
+	}
+
 	@After
 	public void tearDownAfterClass() throws Exception {
 		empresaDAO.exclui("89424232000180");
 	}
 }
-
-
-
